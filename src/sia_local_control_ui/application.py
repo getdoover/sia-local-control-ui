3e07704a0d618aca26dcd9dfb8b588659b548d3f
Jarrod <jarrod@span-eng.com>
2025-11-21 19:33:38 +1000

    fix get_ai bug

--- src/sia_local_control_ui/application.py
+++ src/sia_local_control_ui/application.py
@@ -158,7 +158,7 @@ class SiaLocalControlUiApplication(Application):
         
         # p1_slt_state = self.get_tag(f"AI{self.pump_1_selector}", "platform")
         # p2_slt_state = self.get_tag(f"AI{self.pump_2_selector}", "platform")
-        p1_slt_state, p2_slt_state = await self.get_ai(self.pump_1_selector, self.pump_2_selector)
+        p1_slt_state, p2_slt_state = await self.get_ai([self.pump_1_selector, self.pump_2_selector])
         # p2_slt_state = await self.get_ai(self.pump_2_selector)
         
         if p1_slt_state <= 5 and p2_slt_state <= 5:
