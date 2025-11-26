bbe0d36b273a827e14e0b362d655555383795d0f
Jarrod <jarrod@span-eng.com>
2025-11-26 18:17:00 +1000

    changed status bar color range for tank level

--- src/sia_local_control_ui/static/js/dashboard.js
+++ src/sia_local_control_ui/static/js/dashboard.js
@@ -386,9 +386,9 @@ class Dashboard {
         
         // Update color based on percentage
         progressBar.className = 'progress-fill';
-        if (percentage < 25) {
+        if (percentage < 5) {
             progressBar.classList.add('low');
-        } else if (percentage < 75) {
+        } else if (percentage < 25) {
             progressBar.classList.add('medium');
         }
     }
